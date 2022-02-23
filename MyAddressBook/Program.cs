640a41b574836d225cd2f505e38b29ad62c1e993
PraveenVBongale <praveen.bongale98@gmail.com>
2022-02-23 13:38:28 +0530

    New Contacts

---
+++ MyAddressBook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewContact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME - ADDRESS BOOK");
+            AddressBook address = new AddressBook();
+            address.CreateContact();
+            address.Display();
+            Console.ReadKey();
+        }
+    }
+}
