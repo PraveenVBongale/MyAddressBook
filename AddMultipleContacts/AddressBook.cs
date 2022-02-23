c2856a4ecb7641665a40320885260dbdfe4e3b3a
PraveenVBongale <praveen.bongale98@gmail.com>
2022-02-23 13:51:19 +0530

    Add Multi Contacts

---
+++ AddMultipleContacts/AddressBook.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddMultipleContacts
+{
+    internal class AddressBook
+    {
+        public Dictionary<string, Contact> contacts;
+
+        public AddressBook()
+        {
+            contacts = new Dictionary<string, Contact>();
+        }
+
+        public void CreateContact()
+        {
+            Contact tempContact = new Contact();
+            tempContact.GetUserInfo();
+            string name = tempContact.GetName();
+            if (contacts.ContainsKey(name) is false)
+            {
+                contacts.Add(name, tempContact);
+            }
+            else
+            {
+                Console.WriteLine("erorr");
+            }
+        }
+
+        public void AddContacts()
+        {
+            Contact tempContact = new Contact();
+            tempContact.GetUserInfo();
+            string name = tempContact.GetName();
+            if (contacts.ContainsKey(name) is false)
+            {
+                contacts.Add(name, tempContact);
+            }
+            else
+            {
+                Console.WriteLine("erorr");
+            }
+        }
+
+
+        public void EditContacts()
+        {
+            Contact tempContact = new Contact();
+            tempContact.GetUserInfo();
+            string name = tempContact.GetName();
+            if (contacts.ContainsKey(name) is false)
+            {
+                contacts.Add(name, tempContact);
+            }
+            else
+            {
+                Console.WriteLine("erorr");
+            }
+        }
+
+        public void DeleteContacts()
+        {
+            Contact tempContact = new Contact();
+            tempContact.GetUserInfo();
+            string name = tempContact.GetName();
+            if (contacts.ContainsKey(name) is false)
+            {
+                contacts.Add(name, tempContact);
+            }
+            else
+            {
+                Console.WriteLine("erorr");
+            }
+        }
+
+        public void AddMultiple()
+        {
+            Console.WriteLine("Enter no of contacts to add");
+            int count = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                CreateContact();
+            }
+            Display();
+            Console.WriteLine("Successfully Added New Contacts");
+        }
+        public void Display()
+        {
+            foreach (string name in contacts.Keys)
+                contacts[name].Display();
+        }
+    }
+}
