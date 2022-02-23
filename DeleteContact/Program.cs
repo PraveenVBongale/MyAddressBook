18ab8ed41eeefaade4e5905970a58f1a81020366
PraveenVBongale <praveen.bongale98@gmail.com>
2022-02-23 13:49:03 +0530

    Delete Contacts

---
+++ DeleteContact/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteContact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME - ADDRESS BOOK");
+            AddressBook address = new AddressBook();
+            //address.CreateContact();
+            //address.Display();
+            //Console.ReadKey();
+
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter a Number to Execute the Address Book Program \n1.Create \n2.Add \n3.Edit \n4.Quit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        address.CreateContact();
+                        address.Display();
+                        break;
+
+                    case 2:
+                        address.AddContacts();
+                        address.Display();
+                        break;
+
+                    case 3:
+                        address.EditContacts();
+                        address.Display();
+                        break;
+
+                    case 4:
+                        address.DeleteContacts();
+                        address.Display();
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Invalid");
+                        break;
+                }
+            }
+
+        }
+    }
+}
